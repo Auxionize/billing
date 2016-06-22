c0fc6f062987a9f018c2a71ccb5e27ab64d36a6a
Yordan Georgiev <iordan340@gmail.com>
2016-06-22 15:20:58 +0300

    In progress

--- index.js
+++ index.js
@@ -3,7 +3,19 @@
  */
 'use strict';
 
-module.exports = function() {
-	// todo expose all models
+module.exports = function(sequelize) {
+	let CompanySubscription = require('./lib/models/CompanySubscription')(sequelize);
+	let CompanySubscriptionTransaction = require('./lib/models/CompanySubscriptionTransaction')(sequelize);
+	let Invoice = require('./lib/models/Invoice')(sequelize);
+	let Transaction = require('./lib/models/Transaction')(sequelize);
+	let Wallet = require('./lib/models/Wallet')(sequelize);
+
+	return {
+		CompanySubscription,
+		CompanySubscriptionTransaction,
+		Invoice,
+		Transaction
+	};
+
 };
 
