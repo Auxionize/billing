c96a7266f6eacac066953d9046b38eeeeed62c63
Kristian Tachev <kristian.tachev@auxionize.com>
2017-02-28 14:46:45 +0200

    - Added billingEnums module. - Imported module inside index.js - Removed unused functionality for processing enums object.

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 'use strict';
 
 const _ = require('lodash');
-const processEnumObject = require('./lib/utils/enum').processEnumObject;
+const billingEnums = require('./lib/utils/billingEnums');
+
 const models = [
 	'CompanySubscription',
 	'CompanySubscriptionTransaction',
@@ -12,40 +13,17 @@ const models = [
 	'Transaction',
 	'Wallet'
 ];
+
 const modelsPath = './lib/models/';
 
-module.exports = function(sequelize, config) {
+let billingModule = function(sequelize, config) {
 	let subscriptionUtil = require('./lib/utils/subscriptionUtil');
-	let CompanySubscriptionStatus = {Pending: '', Active: '', Expired: ''};
-	let InvoiceType = {Proforma: '', Invoice: '', CreditNote: ''};
-	let InvoiceStatus = {Active: '', Canceled: ''};
-	let TransactionType = {Proforma: '', Invoice: ''};
-	let TransactionSide = {Charge: '', Payment: ''};
-	let TransactionLinkType = {Auction: ''};
-	let PaymentType = {PAYPAL: '', EPAY: '', CARD: '', BANK_WIRE: '', OTHER: ''};
-
-	processEnumObject(CompanySubscriptionStatus);
-	processEnumObject(InvoiceType);
-	processEnumObject(InvoiceStatus);
-	processEnumObject(TransactionType);
-	processEnumObject(TransactionSide);
-	processEnumObject(TransactionLinkType);
-	processEnumObject(PaymentType);
-
-	let enums = {
-		CompanySubscriptionStatus,
-		InvoiceType,
-		InvoiceStatus,
-		TransactionType,
-		TransactionSide,
-		TransactionLinkType,
-		PaymentType
-	};
-	let exportObject = {subscriptionUtil, enums};
+	
+	let exportObject = {subscriptionUtil, billingEnums};
 
 	if(!_.isUndefined(sequelize)) {
 		_.forEach(models, function(modelName) {
-			exportObject[modelName] = require(`${modelsPath}${modelName}`)(sequelize, enums, config);
+			exportObject[modelName] = require(`${modelsPath}${modelName}`)(sequelize, billingEnums, config);
 		});
 
 	}
@@ -54,3 +32,6 @@ module.exports = function(sequelize, config) {
 
 };
 
+billingModule.enums = billingEnums;
+
+module.exports = billingModule;
